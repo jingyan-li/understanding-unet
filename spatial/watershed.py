f067983d9d1a6aebb5e3a41b9c2ccdb2067edadf
Jingyan Li <jingyli@eu-login-17.euler.ethz.ch>
2022-05-10 23:46:20 +0200

    restructure watershed

--- spatial/watershed.py
+++ spatial/watershed.py
@@ -7,47 +7,20 @@ import random as rng
 import argparse
 from tqdm import tqdm
 
-rng.seed(12345)
-
-a = argparse.ArgumentParser(description='conduct watershed')
-a.add_argument('--attribution_root',
-               default='/cluster/scratch/jingyli/interpretable/attribution_result', type=str,
-               help='root path to attribution pickle')
-a.add_argument('--figure_log_root',
-               default='/cluster/scratch/jingyli/interpretable/visualization', type=str,
-               help='root path to save figures')
-a.add_argument('--binary_thred', default=10, type=int,
-               help='threshold to binarize attribution map in watershed [0,255]')
-a.add_argument('--dist_peak_thred', default=0.5, type=float,
-               help='threshold to filter distance peak as markers in watershed [0,1]')
-a.add_argument('--feat_index', default=11, type=int,
-               help='which feature to be conducted watershed [0,18]')
-a.add_argument('--save_pickle', default=True, type=bool,
-               help='whether to save watershed and attr map as .npy')
-a.add_argument('--attr_to_log', default=True, type=bool,
-               help='whether to take log of attribution level')
-args = a.parse_args()
-
-# path
-attribution_root = Path(args.attribution_root)
-figure_log_root = Path(args.figure_log_root)
-
 
 # functions to read attribution and format attribution matrix
-def read_attr(DATE, TIME, CHANNEL, W, MODEL):
+def read_attr(DATE, TIME, CHANNEL, W, MODEL, attribution_root:Path, figure_log_root:Path):
     file_path = f"{DATE}_berlin_9ch{TIME}-saliency-target-channel{CHANNEL}-W{W}.npy"
     log_root = attribution_root / f"{MODEL}" / f"{DATE}_{TIME}"
     if not os.path.exists(log_root / file_path):
         return None, None
-    try:
-        attr = np.load(log_root / file_path)[0]
-        print(attr.shape)
-        figure_log_path = figure_log_root / f"{MODEL}" / f"{DATE}_{TIME}" / "watershed"
-        if not os.path.exists(figure_log_path):
-            os.makedirs(figure_log_path)
-        return attr, figure_log_path
-    except:
-        return None, None
+
+    attr = np.load(log_root / file_path)[0]
+    figure_log_path = figure_log_root / f"{MODEL}" / f"{DATE}_{TIME}" / "watershed"
+    if not os.path.exists(figure_log_path):
+        os.makedirs(figure_log_path)
+    return attr, figure_log_path
+
 
 def get_space_mat(attr):
     # Get contribution per timeepoch
@@ -203,51 +176,84 @@ def plot_attr_over_watershed(attr, markers, contours, ax):
     ax.set_title(f"Watershed segmentations: {len(contours)}")
 
 
+def main(MODEL, DATE, TIME, CHANNEL, W,
+         attribution_root, figure_log_root, binary_thred, dist_peak_thred,
+            feat_index, save_pickle, attr_to_log
+         ):
+    attr, figure_log_path = read_attr(DATE, TIME, CHANNEL, W, MODEL, attribution_root, figure_log_root)
+    if attr is None:
+        return
+
+    filename = f"{DATE}_{TIME}_C{CHANNEL}-W{W}" if feat_index == 11 else f"{DATE}_{TIME}_C{CHANNEL}-W{W}-feat{feat_index}"
+
+    space_mat = get_space_mat(attr)
+    # of shape [19,512,448]
+    # 19 = 12 timeepochs + 7 static features
+
+    space_mat_ = normalize_space_mat(space_mat, log_norm=attr_to_log)
+
+    # Dimension for watershed
+    n = args.feat_index
+    select_dim_attr = space_mat_[:, :, n:n + 1]
+
+    # ##### Watershed #######
+
+    # Parameters
+    BINARY_THRED = binary_thred
+    DIST_PEAK_THRED = dist_peak_thred
+
+    mark, contours = watershed(select_dim_attr, binary_thred=BINARY_THRED, dist_peak_thred=DIST_PEAK_THRED,
+                               visualize_final=False)
+    # print(f"WATERSHED: detected {len(contours)}")
+
+    if save_pickle:
+        np.save(figure_log_path / filename,
+                np.concatenate((mark[..., np.newaxis], select_dim_attr), axis=-1))
+
+    # Visualize
+    fig, axes = plt.subplots(1, 2, figsize=(20, 10), sharey=True)
+    plot_attr_map(select_dim_attr, axes[0])
+    plot_attr_over_watershed(select_dim_attr, mark, contours, axes[1])
+    plt.savefig(figure_log_path / f"{filename}.png", bbox_inches='tight')
+    plt.close(fig)
+    del fig, axes
+
+
 if __name__ == "__main__":
+    rng.seed(12345)
+
+    a = argparse.ArgumentParser(description='conduct watershed')
+    a.add_argument('--attribution_root',
+                   default='/Users/aaaje/Documents/ETH_WORK/interpretability/data_intermediate/attribution', type=str,
+                   help='root path to attribution pickle')
+    a.add_argument('--figure_log_root',
+                   default='/Users/aaaje/Documents/ETH_WORK/interpretability/data_intermediate/visualization', type=str,
+                   help='root path to save figures')
+    a.add_argument('--binary_thred', default=10, type=int,
+                   help='threshold to binarize attribution map in watershed [0,255]')
+    a.add_argument('--dist_peak_thred', default=0.5, type=float,
+                   help='threshold to filter distance peak as markers in watershed [0,1]')
+    a.add_argument('--feat_index', default=11, type=int,
+                   help='which feature to be conducted watershed [0,18]')
+    a.add_argument('--save_pickle', default=True, type=bool,
+                   help='whether to save watershed and attr map as .npy')
+    a.add_argument('--attr_to_log', default=True, type=bool,
+                   help='whether to take log of attribution level')
+    args = a.parse_args()
+
+    # path
+    attribution_root = Path(args.attribution_root)
+    figure_log_root = Path(args.figure_log_root)
+
     print(args)
 
     MODEL = "resUnet"
     DATE = "2019-07-01"
     TIME = 144
     CHANNEL = 0
-
+    
     for i in tqdm(range(22)):
         for j in range(22):
             W = f"{i}_{j}"
-            attr, figure_log_path = read_attr(DATE, TIME, CHANNEL, W, MODEL)
-            if attr is None:
-                continue
-
-            filename = f"{DATE}_{TIME}_C{CHANNEL}-W{W}" if args.feat_index == 11 else f"{DATE}_{TIME}_C{CHANNEL}-W{W}-feat{args.feat_index}"
-
-            space_mat = get_space_mat(attr)
-            # of shape [19,512,448]
-            # 19 = 12 timeepochs + 7 static features
-
-            space_mat_ = normalize_space_mat(space_mat, log_norm=args.attr_to_log)
-
-            # Dimension for watershed
-            n = args.feat_index
-            select_dim_attr = space_mat_[:, :, n:n + 1]
-
-            # ##### Watershed #######
-
-            # Parameters
-            BINARY_THRED = args.binary_thred
-            DIST_PEAK_THRED = args.dist_peak_thred
-
-            mark, contours = watershed(select_dim_attr, binary_thred=BINARY_THRED, dist_peak_thred=DIST_PEAK_THRED,
-                                       visualize_final=False)
-            # print(f"WATERSHED: detected {len(contours)}")
-
-            if args.save_pickle:
-                np.save(figure_log_path / filename,
-                        np.concatenate((mark[..., np.newaxis], select_dim_attr), axis=-1))
-
-            # Visualize
-            fig, axes = plt.subplots(1, 2, figsize=(20, 10), sharey=True)
-            plot_attr_map(select_dim_attr, axes[0])
-            plot_attr_over_watershed(select_dim_attr, mark, contours, axes[1])
-            plt.savefig(figure_log_path / f"{filename}.png", bbox_inches='tight')
-            plt.close(fig)
-            del fig, axes
+            main(MODEL, DATE, TIME, CHANNEL, W, **args)
+            
---
+++ utils/convert_format.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def npy_to_png():
+    return
